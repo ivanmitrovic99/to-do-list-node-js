f64256f3e42daa8aa00d9f4f7d24d629d21da51b
ivanmitrovic99 <ivanmitrovicenemy@gmail.com>
2023-12-07 18:48:54 +0100

    Minor change.

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,6 +1,8 @@
 const AppError = require("../utils/AppError");
 
 const sendError = (err, req, res) => {
+  console.log(err);
+
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
