f2ff3796476384ad41e89fd7f7e7d95a210eaf5a
ivanmitrovic99 <ivanmitrovicenemy@gmail.com>
2023-12-07 18:39:55 +0100

    Preparing for deployment

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ require("dotenv").config();
 
 const app = require("./app");
 
-const DB = process.env.DATABASE_CONNECTION_STRING.replace("<password>", process.env.DATABASE_PASSWORD);
+mongoose.connect(process.env.MONGO_URI).then(() => console.log("Database connection successfull"));
 
-mongoose.connect(DB).then(() => console.log("Database connection successfull"));
-
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 const server = app.listen(port, () => console.log(`App running on ${port}`));
